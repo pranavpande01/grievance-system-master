594e34d00a4368e179c8a147e3c2e9273da1fc65
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-09 19:03:57 +0000

    first commit

--- frontend.py
+++ frontend.py
@@ -1,14 +1,30 @@
 import streamlit as st
+from uuid import uuid4
 
-
-if "messages" not in st.session_state:
+if "messages" and "sessions" not in st.session_state:
     st.session_state["messages"]=[]
+
 
 def refresh_chats(messages):
     for i in messages:
         with st.chat_message(i["role"]):
             st.text(i["content"])
+def refresh_sidebar(messages):
+    for i in messages:
+        st.button(i["thread_id"])
 
 if user_message:=st.chat_input("type here..."):
-    st.session_state["messages"].append({"role":"user","content":user_message})
+    st.session_state["messages"].append({"thread_id":1,"role":"user","content":user_message})
     refresh_chats(st.session_state["messages"])
+
+
+#st.sidebar.title("hi")
+    
+if st.sidebar.button("new chat"):
+    #send distress signal
+    #clear session
+    st.session_state["messages"]=[]
+    refresh_chats(st.session_state["messages"])
+
+    
+
