0c41cbe57585ad9c115ec64466482c067f9e966b
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-09 19:21:31 +0000

    first commit

--- frontend.py
+++ frontend.py
@@ -1,30 +1,38 @@
 import streamlit as st
 from uuid import uuid4
 
-if "messages" and "sessions" not in st.session_state:
+if "messages" and "curr_thread_id" not in st.session_state:
     st.session_state["messages"]=[]
+    st.session_state["curr_thread_id"]=str(uuid4())
 
 
 def refresh_chats(messages):
     for i in messages:
-        with st.chat_message(i["role"]):
-            st.text(i["content"])
+        if i["thread_id"]==st.session_state["curr_thread_id"]:
+            with st.chat_message(i["role"]):
+                st.text(i["content"])
+
 def refresh_sidebar(messages):
-    for i in messages:
-        st.button(i["thread_id"])
+    for i in set([message['thread_id'] for message in messages]):
+        if st.sidebar.button(i):
+            print("okay")
 
 if user_message:=st.chat_input("type here..."):
-    st.session_state["messages"].append({"thread_id":1,"role":"user","content":user_message})
+    st.session_state["messages"].append({"thread_id":st.session_state["curr_thread_id"],"role":"user","content":user_message})
     refresh_chats(st.session_state["messages"])
+    refresh_sidebar(st.session_state["messages"])
 
 
 #st.sidebar.title("hi")
     
 if st.sidebar.button("new chat"):
     #send distress signal
+
     #clear session
-    st.session_state["messages"]=[]
+    #st.session_state["messages"]=[]
+    st.session_state["curr_thread_id"]=str(uuid4())
     refresh_chats(st.session_state["messages"])
+    refresh_sidebar(st.session_state["messages"])
 
     
 
