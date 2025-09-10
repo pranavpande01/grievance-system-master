fd0d8bff65877035c8b512809b71b30e32932469
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-10 22:43:18 +0530

    fourth commit

--- mail_watcher.py
+++ mail_watcher.py
@@ -10,7 +10,7 @@ EMAIL_ADDRESS=os.getenv("EMAIL_USER")
 covered_mails=[]
 while True:
     with MailBox("imap.gmail.com").login(EMAIL_ADDRESS, PASSWORD,"Inbox") as mb:
-        for i in mb.fetch(criteria='FROM "f20212785@pilani.bits-pilani.ac.in" UNSEEN', mark_seen=True): 
+        for i in mb.fetch(criteria='FROM f"{os.getenv("EMAIL_TO_TRACK")}" UNSEEN', mark_seen=True): 
             pong=ping(f"Email from Neha with body {i.text}")
             print(pong['messages'][-1].content)
             time.sleep(5)
